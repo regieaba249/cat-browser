1562277bca647627ff000157aca290f68b9498bc
Regie Angelo Aba <regie@hxworks.com>
2020-01-18 23:58:23 +0800

    Restructured to store API requested list of breeds to top component
    * Only fetch breeds once on App component mount for optimization.
    * Store selected breed to parent component to retain value on component reload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class BreedSearch extends React.Component {
+
+  renderOption(item, selected) {
+    return (
+      <option key={item.value} value={item.value}>
+        {item.display}
+      </option>
+    );
+  }
+
+  render() {
+    let breeds = this.props.breeds
+    let handler = this.props.handler;
+    let selected = this.props.selected;
+
+    return (
+      <div className="row">
+        <div className="col-md-3 col-sm-6 col-12">
+          <div className="form-group">
+            <label className="form-label" htmlFor="breed">Breed</label>
+            <select
+              id="breed"
+              className="form-control"
+              disabled={breeds.length === 0}
+              onChange={ e => handler(e.target.value) }
+              value={selected}
+            >
+              <option key='' value=''>Select breed</option>
+              {breeds.map((item) => this.renderOption(item))}
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Home extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          selected: '',
+      };
+      this.BreedChange = this.handleBreedChange.bind(this)
+  }
+
+  handleBreedChange(breedID) {
+    this.setState({selected: breedID});
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <div className="container">
+          <h1>Cat Browser</h1>
+          <BreedSearch
+            selected={this.state.selected}
+            breeds={this.props.breeds}
+            handler={this.BreedChange}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
@@ -11,7 +78,7 @@ class BreedSearch extends React.Component {
   }
 
   componentDidMount() {
-    fetch("https://api.thecatapi.com/v1/breeds")
+    fetch("https://api.thecatapi.com/v1/breeds?limit=3")
       .then(response => {
         return response.json();
       })
@@ -20,7 +87,7 @@ class BreedSearch extends React.Component {
           return {value: breed.id, display: breed.name}
         });
         this.setState({
-          breeds: breedsFromApi
+          breeds: breedsFromApi,
         });
       })
       .catch(error => {
@@ -30,28 +97,16 @@ class BreedSearch extends React.Component {
 
   render() {
     return (
-      <div>
-        <select>
-          <option key='' value=''>Select breed</option>
-          {this.state.breeds.map((item) => <option key={item.value} value={item.value}>{item.display}</option>)}
-        </select>
-      </div>
-    )
-  }
-}
-
-function CatBreedSearch(props) {
-  return (
-      <div className="cat-browser">
-        <h1>Cat Browserssss</h1>
-        <BreedSearch />
+      <div className="App">
+        <Home breeds={this.state.breeds}/>
       </div>
     );
+  }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <CatBreedSearch />,
+  <App />,
   document.getElementById('root')
 );
